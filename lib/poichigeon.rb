498ba0cfa66e48b1325b36eddf9b4054eca0eb8b
vianney.sonneville <vianney.sonneville@unova.fr>
2024-10-21 14:13:45 +0200

    rubocop

---
+++ lib/poichigeon.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "poichigeon/version"
+
+module Poichigeon
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/poichigeon/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Poichigeon
+  VERSION = "0.1.0"
+end
